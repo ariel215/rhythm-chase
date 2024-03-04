022a2c2d278e68d542b27722ba3d172fae8e8e5a
ariel <ariel610@proton.me>
2024-03-04 13:52:24 -0600

    tiles can blink!

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,94 @@
+use std::{collections::HashSet, usize};
+
 use raylib::prelude::*;
 use array2d::Array2D;
 
 #[derive(Debug, Default, Clone)]
 pub struct Tile {
-    pub color: Color, 
+    color: Color, 
+    rhythm: Option<TileRhythm>
     // todo: add more features
 }
 
 impl Tile {
-    pub fn from(color: &Color) -> Self{
-        Tile { color: *color}
+    pub fn from(color: &Color, rhythm: Option<TileRhythm> ) -> Self{
+        Tile {
+            color: *color,
+            rhythm,
+        }
+    }
+    pub fn update(&mut self, delta: Sec){
+        self.rhythm.as_mut().map(|r|r.update(delta));
+    }
+
+    pub fn get_color(&self) -> Color {
+        match &self.rhythm {
+            None => self.color,
+            Some(tile_rhythm) => {
+                if tile_rhythm.on() {
+                    self.color
+                } else {
+                    Color::new(0, 0, 0, 0)
+                }
+            }
+        }
     }
 }
+
+pub type Sec = f64;
+pub type BPM = f64;
+
+pub fn beat_length(tempo: BPM) -> Sec {
+    60_f64 / tempo as Sec
+}
+
+
+#[derive(Debug,Default,Clone)]
+pub struct TileRhythm {
+    // Number of beats in a measure
+    length: usize,
+    // Length of a beat, in seconds
+    duration: Sec,
+    // which beats to play; zero-indexed
+    beats: HashSet<usize>,
+    // 
+    time: Sec,
+}
+
+impl TileRhythm{
+    pub fn update(&mut self, delta: Sec){
+        self.time += delta;
+        self.time = self.time % (self.duration * self.length as Sec);
+    }
+
+    pub fn on(&self) -> bool {
+        self.beats.contains(& ((self.time / self.duration) as usize))
+    }
+    pub fn new<T>(length: usize, tempo: BPM, beats: T) -> Self 
+    where T: IntoIterator<Item=usize> {
+        TileRhythm{
+            length, 
+            duration: beat_length(tempo),
+            beats: beats.into_iter().collect(),
+            time: 0.0
+        }
+    }
+}
+
+#[test]
+fn test_rhythm(){
+    let mut r = TileRhythm::new(2, 120.0, vec![1]);
+    assert_eq!(r.on(), false);
+    r.update(0.15);
+    assert_eq!(r.on(), false);
+    assert!((r.time - 0.15).abs() < 1e-6);
+    r.update(r.duration);
+    assert!(r.on());
+    r.update(r.duration);
+    assert_eq!(r.on(), false);
+    assert!((r.time - 0.15).abs() < 1e-6);
+}
+
 
 pub struct Level {
     pub tiles: Array2D<Tile>,
@@ -33,15 +110,20 @@ impl Level {
         }
     }
     pub fn draw(&self, handle: &mut RaylibDrawHandle){
-        for row in 0..self.tiles.row_len(){
-            for col in 0..self.tiles.column_len(){
-                handle.draw_rectangle(row as i32 * self.tile_width + self.row_gap / 2,
-                    col as i32 * self.tile_height + self.column_gap / 2,
-                    self.tile_width - self.row_gap / 2,
-                    self.tile_height - self.column_gap / 2,
-                    self.tiles[(row,col)].color
-                );
-            }
+        for ((row,col), tile) in self.tiles.enumerate_column_major() {
+            handle.draw_rectangle(row as i32 * self.tile_width + self.row_gap / 2,
+                col as i32 * self.tile_height + self.column_gap / 2,
+                self.tile_width - self.row_gap / 2,
+                self.tile_height - self.column_gap / 2,
+                tile.get_color()
+            );
+        }
+    }
+
+    pub fn update(&mut self, delta: Sec){
+        for (r,c) in self.tiles.indices_column_major(){
+            let tile = self.tiles.get_mut(r, c).unwrap();
+            tile.update(delta)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
 
 use raylib::prelude::*;
+use std::time::{self, *};
 use rhythm_chase::*;
 use array2d::{Array2D, Error};
 
@@ -10,27 +11,31 @@ fn main() -> Result<(), Error>{
         .size(w,h)
         .title("Checkerboard")
         .build();
-     
-    let row1 = (vec![Color::RED, Color::BLACK, Color::RED, Color::BLACK, Color::RED, Color::BLACK, Color::RED, Color::BLACK,])
-        .iter().map(Tile::from).collect::<Vec<_>>();
-    let row2 = (vec![Color::BLACK, Color::RED, Color::BLACK, Color::RED, Color::BLACK, Color::RED, Color::BLACK, Color::RED])
-    .iter().map(Tile::from).collect::<Vec<_>>();
-let rows = vec![row1.clone(), row2.clone(), row1.clone(), row2.clone(), row1.clone(), row2.clone(), row1.clone(), row2.clone()];
+    let downbeat = TileRhythm::new(2, 120., [0]);
+    let upbeat = TileRhythm::new(2,120.,[1]);
+    let red = Tile::from(&Color::RED, Some(downbeat));
+    let black: Tile = Tile::from(&Color::BLACK, Some(upbeat));
+    
+    let row1 = vec![red.clone(), black.clone(), red.clone(), black.clone(), red.clone(), black.clone(), red.clone(), black.clone()];
+    let row2 = vec![black.clone(), red.clone(), black.clone(), red.clone(), black.clone(), red.clone(), black.clone(), red.clone()];
+    let rows = vec![row1.clone(), row2.clone(), row1.clone(), row2.clone(), row1.clone(), row2.clone(), row1.clone(), row2.clone()];
     let rows = Array2D::from_rows(&rows)?;
     let margin = 6;
-    let level = Level::new(
+    let mut level = Level::new(
         rows,
         w / 8,
         h / 8,
         margin,
         margin
     );
+    let mut time = SystemTime::now();
     while !rl.window_should_close() {
+        let duration = SystemTime::now().duration_since(time).unwrap();
+        time = SystemTime::now();
         let mut d = rl.begin_drawing(&thread);
-         
         d.clear_background(Color::WHITE);
+        level.update(duration.as_secs_f64());
         level.draw(&mut d);
-        
     }
     Ok(())
 }
